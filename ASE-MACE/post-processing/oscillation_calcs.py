1e4439ff74549e331651e53d745cb02a3d74b1b9
lclemens05 <136498602+lclemens05@users.noreply.github.com>
2025-11-30 10:13:17 -0500

    update

--- ASE-MACE/post-processing/oscillation_calcs.py
+++ ASE-MACE/post-processing/oscillation_calcs.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from ase.io import read, write
 
 # load traj file
-trajectory = read('/projectnb/me508/students/lclemens/ASE-MACE/OLDproduction_nvt_ljc.traj', index=":")
+trajectory = read('/projectnb/me508/students/krtung/project/ASE/production_nvt_RT.traj', index=":")
 
 num_frames = len(trajectory)
 # just in case your traj file is bad
@@ -14,12 +15,26 @@ print(f"Loaded {num_frames} frames from the trajectory.")
 reference = trajectory[0].get_positions()   # use first frame as reference
 disp_per_frame = [] # to store displacements
 
-max_frame = 1000001  # limit to first 20,000 frames; this is arbitrary and can change
-print(f"Calculating displacements for the first {min(max_frame, num_frames)} frames")
-for frame in trajectory[1:max_frame]:    # loop over each frame (only some subset of the beginning), start from 1 to not include the first
+#max_frame = 20000   # limit to first 20,000 frames
+#print(f"Calculating displacements for the first {min(max_frame, num_frames)} frames")
+for frame in trajectory[1:]:    # loop over each frame (only some subset of the beginning), start from 1 to not include the first
     disp = np.linalg.norm(frame.get_positions() - reference, axis=1)    # calculate displacement from reference for each atom; this is magnitude of displacement so already positive
     disp_per_frame.append(disp.mean()) # list of average displacements per frame (so each element is from one frame and it's the average of all atoms)
 
 avg_disp_per_frame = np.array(disp_per_frame)
 overall_avg_disp = avg_disp_per_frame.mean()  # overall average displacement across all frames
 print(f"\nOverall average displacement from reference structure: {overall_avg_disp:.4f} A")
+
+# Plot average displacement vs frame index
+plt.figure(figsize=(8,5))
+plt.plot(avg_disp_per_frame, 'o', markersize=.5, label='Average displacement')
+plt.xlabel('Frame')
+plt.ylabel('Average Euclidean Distance (Å)')
+plt.title('298K')   # Change for each temp
+plt.grid(True)
+
+# Save figure
+plt.savefig("avg_distances_vs_frame.png", dpi=300, bbox_inches='tight')
+plt.close()
+
+print("Saved plot: avg_distances_vs_frame.png")
